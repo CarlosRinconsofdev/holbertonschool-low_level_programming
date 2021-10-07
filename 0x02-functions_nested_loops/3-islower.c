1a5364124c66d31f8e7d5faa0f657eba0c141e30
Carlos Rincón <3901@holbertonschool.com>
2021-10-07 22:44:02 +0000

    project c 0x02

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Check the code
+ * @c: its de variable in
+ * Return: 0 - 1
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z') 
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
