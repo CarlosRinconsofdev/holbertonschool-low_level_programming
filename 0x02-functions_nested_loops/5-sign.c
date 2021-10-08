ca307dda996a9e4ac1d4c89b72ed6df1edaa8f46
Carlos Rincón <3901@holbertonschool.com>
2021-10-08 05:45:09 +0000

    project c 0x02

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,27 @@
 
 /**
  * print_sign - Check the code
- * @n: Variable int
+ * @n: Variable in
  * Return: -1, 0, 1
  */
 int print_sign(int n)
 {
+int number;
+
 if (n > 0)
 {
-return (1);
-_putchar ('+');
+number = 1;
+_putchar('+');
 }
-if (n == 0)
+else if (n == 0)
 {
-return (0);
+number = 0;
 _putchar(0);
 }
-if (n < 0)
+else
 {
-return (-1);
-_putchar ('-');
+number = -1;
+_putchar('-');
 }
+return (number);
 }
