ab9ab15b326bd9e45298c1bb728db2ca6bff2aad
Carlos Rincón <3901@holbertonschool.com>
2021-10-14 23:34:23 +0000

    project c 0x02

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,17 @@ void print_to_98(int n)
 int r;
 if (n < 98)
 {
-for (r = n; r <= 98; r++)
-{
-printf("%i, ", r);
-}
-{
-printf("\n");
-}
-if (n == 98)
-{
-printf("%i, ", 'n');
-}
-if (n > 98)
-{
-for (r = n; r >= 98; r--)
+for (r = n; r <= 97; r++)
 {
 printf("%i, ", r);
 }
 }
+else
+{
+for (r = n; r > 98; r--)
+{
+printf("%i, ", r);
 }
+}
+printf("98\n");
 }
