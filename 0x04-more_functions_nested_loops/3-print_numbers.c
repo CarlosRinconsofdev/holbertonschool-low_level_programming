f293eb83e8f2fc425bddb7e1f5f9a5d5e549fd60
Carlos Rincón <3901@holbertonschool.com>
2021-10-16 21:06:29 +0000

    project 0x04

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * 
+ * Return: No
+ */
+void print_numbers(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+_putchar(n);
+}
+ _putchar('\n');
+}
