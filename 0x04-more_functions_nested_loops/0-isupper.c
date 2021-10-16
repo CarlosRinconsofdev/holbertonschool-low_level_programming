f1b6aaa7eeb508b29e5f06d6137ab397274881c4
Carlos Rincón <3901@holbertonschool.com>
2021-10-16 04:20:43 +0000

    project 0x04

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - prototype check the code
+ * @c: variable 
+ * Return: 1 uppercase 0 not uppercase
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
