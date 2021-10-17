d062afe93182f8b3bb9360d7aaafe25bec14c2d6
Carlos Rincón <3901@holbertonschool.com>
2021-10-17 05:39:28 +0000

    project c 0x04

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - prototype
+ * 
+ * Return: void
+ */
+void print_square(int size)
+{
+int i, s;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (s = 0; s < size; s++)
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
