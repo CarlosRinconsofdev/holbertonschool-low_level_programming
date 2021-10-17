37f9497e456db673c7956785b58c3fa106747889
Carlos Rincón <3901@holbertonschool.com>
2021-10-17 06:10:02 +0000

    project c 0x04

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,14 @@ int main(void)
 int i = 1;
 for (i = 0; i < 100; i++)
 {
-if (i % 3 == 0 && i %5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 printf("fizzBuzz ");
 else if (i % 3 == 0)
 printf("Fizz ");
 else if (i % 5 == 0)
 printf("Buzz ");
 else
-printf("%d ",i);
+printf("%d ", i);
 }
 printf("Buzz\n");
 return (0);
