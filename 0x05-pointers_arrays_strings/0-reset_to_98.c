ef87daeb6d83d5b752349b524a9cbd68974d2281
Carlos Rincón <3901@holbertonschool.com>
2021-10-18 10:26:10 +0000

    project c 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ * @n: pointer
+ * Return: no
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Hello world - Project 0x05-pointers_arrays_strings
