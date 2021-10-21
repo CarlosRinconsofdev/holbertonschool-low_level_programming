5eeeb5cbcb9e8beb2551c7db49a4cc3544960dbf
Carlos Rincón <3901@holbertonschool.com>
2021-10-21 04:03:18 +0000

    project c 0x08

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - check the code
+ * @n: integer
+ * Return: Function the factorial
+ */
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n <= 1)
+return (1);
+return (n * factorial(n - 1));
+}
