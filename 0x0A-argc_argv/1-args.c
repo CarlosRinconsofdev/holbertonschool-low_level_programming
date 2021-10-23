6828422fcf2c019ecbe82a1e056b9d9c5a77524f
Carlos Rincón <3901@holbertonschool.com>
2021-10-23 03:45:44 +0000

    project c 0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: counter
+ * @argv: value
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+if (argv[0])
+printf("%d\n", argc - 1);
+return (0);
+}
