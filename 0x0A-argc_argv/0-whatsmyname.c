786fc9811b77c49086cf076684952e9dd897475a
Carlos Rincón <3901@holbertonschool.com>
2021-10-23 03:36:21 +0000

    project c 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: count
+ * @argv: value
+ * Return: Always 0 (Succes)
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
