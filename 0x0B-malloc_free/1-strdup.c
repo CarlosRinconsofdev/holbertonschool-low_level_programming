4e0896ac5330d6b1e67838d2a492d8a90d1eb0bc
Carlos Rincón <3901@holbertonschool.com>
2021-10-27 03:17:50 +0000

    project c 0x0B

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - check the code
+ * @str: string
+ * Return: Pointer
+ */
+char *_strdup(char *str)
+{
+int length, i;
+char *arr;
+if (str == NULL)
+{
+return (NULL);
+}
+length = 0;
+while (*(str + length) != '\0')
+{
+length++;
+}
+arr = malloc(length * sizeof(char) + 1);
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < length; i++)
+{
+arr[i] = str[i];
+}
+arr[i] = '\0';
+return (arr);
+}
