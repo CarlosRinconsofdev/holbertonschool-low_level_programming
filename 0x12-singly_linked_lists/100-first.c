2b4fd4c1fe11edfe57d4471c06fe55782e28c9c3
CarlosRinconsofdev <3901@holbertonschool.com>
2021-11-12 22:41:36 -0500

    project c 0x12

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void __attribute__((constructor)) turtle(void);
+
+/**
+ * turtle - print function
+ * Return: void
+ */
+
+void turtle(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
