8e2ec65c4b8541ba0271344b2a97089133c6dcc5
CarlosRinconsofdev <3901@holbertonschool.com>
2021-11-16 23:23:35 -0500

    project c 0x13

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t size = 0;
-
-while (h != NULL)
+size_t nodes = 0;
+while (h)
 {
-size++;
 printf("%d\n", h->n);
 h = h->next;
+nodes++;
 }
-return (size);
+return (nodes);
 }
