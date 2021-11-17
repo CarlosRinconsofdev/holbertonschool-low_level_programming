35c913dbd7b3a98f6f25db35f524cf94aea1067a
CarlosRinconsofdev <3901@holbertonschool.com>
2021-11-17 16:56:50 -0500

    project c 0x13

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 listint_t *node;
 unsigned int i;
 if (head == NULL)
+return (NULL);
 node = head;
 i = 0;
 while (node != NULL)
