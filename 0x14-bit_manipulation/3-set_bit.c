7c4f25d28363a702a40c6d23442a3f05180d3151
CarlosRinconsofdev <3901@holbertonschool.com>
2021-11-19 14:17:16 -0500

    project c 0x14

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * set_bit - Set the value of a bit 
+ * set_bit - Set the value of a bit
  * @n: int
  * @index: Unsigned int
  * Return: 1 or -1
@@ -8,7 +8,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int mask;
-if (index >= (sizeof(*n)*8))
+if (index >= (sizeof(*n) * 8))
 return (-1);
 mask = 1;
 *n = *n | (mask << index);
