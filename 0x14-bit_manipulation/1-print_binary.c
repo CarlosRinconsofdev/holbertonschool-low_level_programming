2f8d8c676ed8bd07bbe5c3d234ce8a488569e6b6
CarlosRinconsofdev <3901@holbertonschool.com>
2021-11-18 23:33:09 -0500

    project c 0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_binary - Prints the binary representation of a number
- * @n: int 
+ * @n: int
  * Return: Void
  */
 void print_binary(unsigned long int n)
@@ -16,7 +16,7 @@ while (max)
 {
 if (l == 1 && (n & max) == 0)
 _putchar('0');
-else if ((n & max) !=0)
+else if ((n & max) != 0)
 {
 _putchar('1');
 l = 1;
