7c1874e0477502daf7fe76564aa6d08b27ed80ba
CarlosRinconsofdev <3901@holbertonschool.com>
2021-11-18 23:31:53 -0500

    project c 0x14

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: int 
+ * Return: Void
+ */
+void print_binary(unsigned long int n)
+{
+unsigned int l = 0, max = 32768;
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+while (max)
+{
+if (l == 1 && (n & max) == 0)
+_putchar('0');
+else if ((n & max) !=0)
+{
+_putchar('1');
+l = 1;
+}
+max >>= 1;
+}
+}
