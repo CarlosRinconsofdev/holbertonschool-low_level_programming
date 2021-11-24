6e4f505bc8a9c2332accb5514751617c06f6fe07
CarlosRinconsofdev <3901@holbertonschool.com>
2021-11-23 23:41:34 -0500

    project C 0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,4 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "main.h"
 /**
  * read_textfile - Write aa function that reads a text file and prints it
  * @filename: char
